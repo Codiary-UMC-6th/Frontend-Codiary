8b7f267e6fd721e30ea2b58cd76d3ffaca8e8a3e
kwonjeong <kwonjeong9470@gmail.com>
2024-07-31 17:20:10 +0900

    [Design] #7 - 소셜 로그인 버튼 레이아웃 구현

--- src/components/login/LoginModal.jsx
+++ src/components/login/LoginModal.jsx
@@ -1,31 +1,43 @@
 import React from 'react'
 import styled from "styled-components";
 import * as Color from '../../common/Color';
+import NaverIcon from '../../assets/login/naverIcon.svg';
+import KakaorIcon from '../../assets/login/kakaoIcon.svg';
+import GoogleIcon from '../../assets/login/googleIcon.svg';
+import GithubIcon from '../../assets/login/githubIcon.svg';
+import EmailIcon from '../../assets/login/mailIcon.svg';
+
 
 export const LoginModal = () => {
 
 
   return (
     <St.LoginModalWrapper>
-      <St.LoginModalWrapper>
-        <St.HeaderTitle>CODIARY</St.HeaderTitle>
-        <St.Input
-          type="text"
-          placeholder='input name = "userID"'
-        />
-        <St.Input
-          type="text"
-          placeholder='input name = "Password"'
-        />
-        <St.LoginSettingBox>
-          <St.Checkbox />
-          <St.StayLoginLabel>
-            로그인 유지하기
-          </St.StayLoginLabel>
-          회원이 아니신가요?
-          <St.LinkText href="/signup">회원가입</St.LinkText>
-        </St.LoginSettingBox>
-      </St.LoginModalWrapper>
+      <St.HeaderTitle>CODIARY</St.HeaderTitle>
+      <St.Input
+        type="text"
+        placeholder='input name = "userID"'
+      />
+      <St.Input
+        type="text"
+        placeholder='input name = "Password"'
+      />
+      <St.LoginSettingBox>
+        <St.Checkbox />
+        <St.StayLoginLabel>
+          로그인 유지하기
+        </St.StayLoginLabel>
+        회원이 아니신가요?
+        <St.LinkText href="/signup">회원가입</St.LinkText>
+      </St.LoginSettingBox>
+      <St.LoginButton title='로그인'>로그인</St.LoginButton>
+      <St.ButtonContainer>
+        <St.IconButton><img src={NaverIcon} alt="Naver" /></St.IconButton>
+        <St.IconButton><img src={KakaorIcon} alt="Naver" /></St.IconButton>
+        <St.IconButton><img src={GoogleIcon} alt="Naver" /></St.IconButton>
+        <St.IconButton><img src={GithubIcon} alt="Naver" /></St.IconButton>
+        <St.IconButton><img src={EmailIcon} alt="Naver" /></St.IconButton>
+      </St.ButtonContainer>
     </St.LoginModalWrapper>
   )
 }
@@ -66,10 +78,10 @@ const St = {
     margin-bottom: 24px;
     width: 400px;
     height: 56px;
+    font-wight: 400;
     border: none;
     border-radius: 10px;
     font-size: 18px;
-    font-wight: 400px;
     line-height: 22px;
     outline: none;
     padding-left: 17px;
@@ -121,4 +133,51 @@ const St = {
     text-decoration: underline;
   `,
 
+  LoginButton: styled.button`
+    width: 160px;
+    height: 48px;
+    border: none;
+    border-radius: 15px;
+    margin: 36px auto;
+    background-color: ${Color.primary_blue};
+    text-align: center;
+    color: ${Color.text1};
+    font-family: 'Pretendard';
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 32px;
+    cursor: pointer;
+    
+    &:hover {
+      opacity: 0.8;
+    }
+  `,
+
+  ButtonContainer: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    max-width: 600px;
+`,
+
+  IconButton: styled.button`
+    background: none;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 12px;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    img {
+      width: 32px;
+      height: 32px;
+    }
+`,
 }
