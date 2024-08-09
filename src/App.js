939b11a85c07a76f2abd9b2cb96f35dbb52c68e1
bbakbum <zxc534@naver.com>
2024-08-10 01:00:21 +0900

    [feature] user profile page load info api apply, CORS error handle

--- src/App.js
+++ src/App.js
@@ -15,27 +15,11 @@ import { LoginModal } from "./components/login/LoginModal";
 
 import "./App.css";
 
-import { get, post, put, del } from './common/api'; 
-
 function App() {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
   const openLoginModal = () => setIsLoginModalOpen(true);
   const closeLoginModal = () => setIsLoginModalOpen(false);
-
-  const handleClick = async () => {
-    const data = {
-      email: "zxc534@naver.com",
-      password: "qwer1234"
-    };
-  
-    try {
-      const result = await post('/members/login', data);
-      console.log('POST 요청 결과:', result);
-    } catch (error) {
-      console.error('POST 요청 실패:', error);
-    }
-  }  
 
   return (
     <BrowserRouter>
@@ -43,7 +27,7 @@ function App() {
       <div id="main-content">
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:memberId" element={<Profile />} />
           <Route path="/team" element={<Team />} />
           <Route path="/calendar" element={<Calendar />} />
           <Route path="/diary" element={<Diary />} />
@@ -53,7 +37,6 @@ function App() {
         </Routes>
         <button onClick={openLoginModal}>Login</button>
         {isLoginModalOpen && <LoginModal onClose={closeLoginModal} />}
-        <button onClick={handleClick}>테스트 버튼</button>
       </div>
       <Footer />
     </BrowserRouter>
