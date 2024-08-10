76cf27ab05030e67686b8c5751e2e2bbf9cc5f15
meongdo <rlagustj07@ewha.ac.kr>
2024-08-11 01:24:02 +0900

    [feature] 북마크 추가 api 구현

---
+++ src/components/diaryDetails/FAB.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import * as Color from "../../common/Color";
+
+import { ReactComponent as SharekIcon } from "../../assets/icon_share.svg";
+import BookmarkBtn from "./BookmarkBtn";
+
+const Container = styled.div`
+    background: ${Color.primary_blue};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 80px;
+    height: 180px;
+    border-radius: 40px;
+    gap: 28px;
+
+    position: fixed;
+    top: 158px;
+    left: 130px;
+    z-index: 10;
+`;
+
+export const Button = styled.button`
+    border: 0;
+    background-color: transparent;
+    cursor: pointer;
+`;
+
+const StyledShareIcon = styled(SharekIcon)`
+    &:hover {
+        path {
+            stroke: ${Color.primary_yellow};
+        }
+    }
+`;
+
+const FAB = () => {
+
+    return(
+        <Container>
+            <BookmarkBtn />
+            <Button>
+                <StyledShareIcon />
+            </Button>
+        </Container>
+    );
+}
+
+export default FAB;
