ebe9803bdb1dc4bfdbff4081185b35971ae6c036
kwonjeong <kwonjeong9470@gmail.com>
2024-08-16 10:12:30 +0900

    [Feat] #56 - 상태 스토어에 email, nickname, memberID 추가

--- src/store/LoginStore.js
+++ src/store/LoginStore.js
@@ -3,5 +3,12 @@ import { create } from 'zustand'
 export const useLoginStore = create((set) => ({
     isLogin: false,
     setLogin: () => set({ isLogin: true }),
-    setLogout: () => set({ isLogin: false })
+    setLogout: () => set({ isLogin: false }),
+
+    email: null,
+    setEmail: (email) => set({ email: email }),
+    nickname: null,
+    setNickname: (nickname) => set({ nickname: nickname }),
+    memberId: null,
+    setMemberId: (id) => ({ memberId: id }),
 }));
