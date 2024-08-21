6b4d9b9937e26e9e0bbe2e1a77c4b3a44e30e98e
kwonjeong <kwonjeong9470@gmail.com>
2024-08-21 18:49:31 +0900

    [Feat] #76 - 팀 리스트 api 연결

--- src/components/navbar/Dropdown.jsx
+++ src/components/navbar/Dropdown.jsx
@@ -5,7 +5,6 @@ import { get } from "../../common/api.js";
 
 import EnabledSvg from "../../assets/dropdown-enabled.svg";
 import DisabledSvg from "../../assets/dropdown-disabled.svg";
-import { getElementError } from "@testing-library/react";
 import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
@@ -29,14 +28,44 @@ const DropdownBox = styled.div`
   position: absolute;
   top: 75px;
   min-height: 150px;
+  max-height: 400px; /* 모달 최대높이  */
   width: 180px;
   display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: ${Color.background3};
+  flex-direction: column;
+  background-color: ${Color.backgroundBlur};
+  overflow-y: auto; /* 스크롤 생성 */
+  padding: 10px;
 `;
 
-const TeamBox = styled.div``;
+const TeamBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px; 
+  margin-bottom: 10px;
+`;
+
+const TeamListWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px 10px;
+  background-color: ${Color.backgroundBlur};
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const TeamName = styled.div`
+  color: ${Color.text1};
+  font-family: 'D2Coding';
+  font-size: 18px;
+`;
+
+const TeamColor = styled.div`
+  width: 22px;
+  height: 22px;
+  background-color: #D9D9D9;
+  margin-right: 8px;
+  border-radius: 50%
+`;
 
 const Create = styled.div`
   cursor: pointer;
@@ -46,8 +75,9 @@ const Create = styled.div`
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
-  line-height: 24px; /* 150% */
+  line-height: 24px;
   text-decoration-line: underline;
+  margin-top: ${({ hasTeams }) => (hasTeams ? "auto" : "0")}; /* 팀이 없을 때 중앙에 배치 */
 `;
 
 const Dropdown = () => {
@@ -56,12 +86,12 @@ const Dropdown = () => {
 
   const [teamList, setTeamList] = useState([]);
 
-
   const toggleDropdown = () => {
     setVisibility(!visibility);
   };
 
   const createTeam = () => {
+    setVisibility(false);
     navigate("/teamAdd");
   };
 
@@ -69,12 +99,12 @@ const Dropdown = () => {
     try {
       const response = await get(`/teams/list`);
       const teamList = response.result.teams;
-      console.log('팀 리스트 가져오기 성공', teamList);
+      console.log("팀 리스트 가져오기 성공", teamList);
       setTeamList(teamList);
     } catch (error) {
-      console.error('팀 리스트 가져오기 실패', error);
+      console.error("팀 리스트 가져오기 실패", error);
     }
-  }
+  };
 
   useEffect(() => {
     getTeamList();
@@ -84,23 +114,30 @@ const Dropdown = () => {
     <Container>
       <Nav onClick={toggleDropdown}>
         <span>팀 홈</span>
-        {visibility ? (
-          <Svg src={DisabledSvg}></Svg>
-        ) : (
-          <Svg src={EnabledSvg}></Svg>
-        )}
+        {visibility ? <Svg src={DisabledSvg} /> : <Svg src={EnabledSvg} />}
       </Nav>
-      {visibility ? (
-        <DropdownBox id="DropdownBox">
-          <TeamBox>
-            {teamList.map((team) => {
-              return <></>;
-            })}
-          </TeamBox>
-          <Create onClick={createTeam}>팀스페이스 만들기</Create>
+      {visibility && (
+        <DropdownBox>
+          {teamList.length > 0 ? (
+            <>
+              <TeamBox>
+                {teamList.map((team) => (
+                  <TeamListWrapper key={team.teamId}>
+                    <TeamColor />
+                    <TeamName>{team.teamName}</TeamName>
+                  </TeamListWrapper>
+                ))}
+              </TeamBox>
+              <Create hasTeams={true} onClick={createTeam}>
+                팀스페이스 만들기
+              </Create>
+            </>
+          ) : (
+            <Create hasTeams={false} onClick={createTeam}>
+              팀스페이스 만들기
+            </Create>
+          )}
         </DropdownBox>
-      ) : (
-        <></>
       )}
     </Container>
   );
