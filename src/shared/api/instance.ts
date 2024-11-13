f59872b21ffe474aa13fd32ecfa467c254b0c420
kwonjeong <kwonjeong9470@gmail.com>
2024-11-13 23:19:52 +0900

    #107 setting: 서버통신 초기세팅 코드 추가

---
+++ src/shared/api/instance.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+import { handleAPIError } from './interceptor';
+
+export const axiosInstance = axios.create({
+  baseURL: `${process.env.REACT_APP_BASE_URL}/api/v2`,
+  withCredentials: true,
+
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const axiosPublicInstance = axios.create({
+  baseURL: `${process.env.REACT_APP_BASE_URL}/api/v2`,
+  withCredentials: true,
+
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// axiosInstance.interceptors.request.use(handleCheckAndSetToken);
+
+// axiosInstance.interceptors.response.use((res) => res, handleTokenError);
+
+axiosInstance.interceptors.response.use((res) => res, handleAPIError);
