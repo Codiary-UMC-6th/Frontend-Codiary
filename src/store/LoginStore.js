cc675fb1299466de6b3d3fa68bd123479a949b8c
kwonjeong <kwonjeong9470@gmail.com>
2025-01-05 22:14:55 +0900

    #114 fix: 로그인 및 로그아웃 로직 수정

--- src/store/LoginStore.js
+++ src/store/LoginStore.js
@@ -1,34 +1,35 @@
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export const useLoginStore = create(
-    persist(
-        (set) => ({
-            isLogin: false,
-            setLogin: () => set({ isLogin: true }),
-            setLogout: () => set({ 
-                isLogin: false ,
-                email: null,
-                nickname: null,
-                memberId: null,
-            }),
-        
-            email: null,
-            setEmail: (email) => set({ email: email }),
-            nickname: null,
-            setNickname: (nickname) => set({ nickname: nickname }),
-            memberId: null,
-            setMemberId: (id) => {
-                const memberId = String(id);
-                set({ memberId });
-                console.log(memberId); },
+  persist(
+    (set) => ({
+      isLogin: false,
+      setLogin: () => set({ isLogin: true }),
+      setLogout: () =>
+        set({
+          isLogin: false,
+          email: null,
+          nickname: null,
+          memberId: null,
+        }),
 
-            teamList: [],
-            setTeamList: (list) => set({ teamList: list }),
-            }),
-            {
-                name: "userInfoStorage",
-            }
-    )
+      email: null,
+      setEmail: (email) => set({ email: email }),
+      nickname: null,
+      setNickname: (nickname) => set({ nickname: nickname }),
+      memberId: null,
+      setMemberId: (id) => {
+        const memberId = String(id);
+        set({ memberId });
+        console.log(memberId);
+      },
+
+      teamList: [],
+      setTeamList: (list) => set({ teamList: list }),
+    }),
+    {
+      name: "userInfoStorage",
+    }
+  )
 );
-
